ca35b71dec59715e93194c9fa01f14fd9d25f805
ze-mu-zhou <996029583@qq.com>
2025-03-25 00:38:08 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,3 +1,10 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 fn main() {
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
     println!("cargo:rustc-cfg=feature=\"pass\"");
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 }
