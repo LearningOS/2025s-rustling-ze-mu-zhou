bb3e49bc0e7f14a0d5644bee13e374c6614632e1
ze-mu-zhou <996029583@qq.com>
2025-03-24 23:51:09 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,31 +1,24 @@
-// clippy3.rs
-// 
-// Here's a couple more easy Clippy fixes, so you can see its utility.
-//
-// Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        my_option.unwrap();
+        // 使用 `if let` 或 `match` 处理 `None` 情况
+        println!("my_option is None");
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let mut my_vec = vec![1, 2, 3, 4, 5];
+    my_vec.clear(); // 直接修改原 Vec
+    println!("This Vec is empty, see? {:?}", my_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    // 使用 `std::mem::swap` 交换两个变量的值
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
